2588298adcd429785c47e9285c148618109e2a67
David Richard <david.richard@bull.net>
2012-05-09 09:26:17 +0200

    CLOUD-39 add vhost-manager api

---
+++ vhost-manager/api/src/main/java/org/ow2/jonas/jpaas/apache/manager/vhost/manager/api/VhostManagerService.java
@@ -0,0 +1,194 @@
+/**
+ * JPaaS
+ * Copyright (C) 2012 Bull S.A.S.
+ * Contact: jasmine@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * --------------------------------------------------------------------------
+ * $Id$
+ * --------------------------------------------------------------------------
+ */
+
+package org.ow2.jonas.jpaas.apache.manager.vhost.manager.api;
+
+import java.util.List;
+
+/**
+ * The interface describing the VhostManagerService to manage Virtual Host on an Apache server
+ *
+ * @author David Richard
+ */
+public interface VhostManagerService {
+
+
+    /**
+     * Create a Virtual Host block directive
+     *
+     * @param address address of the virtual host
+     * @throws VhostManagerException
+     */
+    void createVirtualHost(String address) throws VhostManagerException;
+
+    /**
+     * Delete a Virtual Host block directive
+     *
+     * @param address address of the virtual host
+     * @throws VhostManagerException
+     */
+    void deleteVirtualHost(String address) throws VhostManagerException;
+
+    /**
+     * Create a Name-based Virtual Host block directive
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @throws VhostManagerException
+     */
+    void createVirtualHost(String address, String serverName) throws VhostManagerException;
+
+    /**
+     * Delete a Name-based Virtual Host block directive
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @throws VhostManagerException
+     */
+    void deleteVirtualHost(String address, String serverName) throws VhostManagerException;
+
+
+    /**
+     * Create a DocumentRoot directive in a Virtual Host
+     *
+     * @param address address of the virtual host
+     * @param documentRoot value of the DocumentRoot directive to create
+     * @throws VhostManagerException
+     */
+    void createDocumentRoot(String address, String documentRoot) throws VhostManagerException;
+
+
+    /**
+     * Delete a DocumentRoot directive in a Virtual Host
+     *
+     * @param address address of the virtual host
+     * @throws VhostManagerException
+     */
+    void deleteDocumentRoot(String address) throws VhostManagerException;
+
+    /**
+     * Create a DocumentRoot directive in a Name-based Virtual Host
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @param documentRoot value of the DocumentRoot directive to create
+     * @throws VhostManagerException
+     */
+    void createDocumentRoot(String address, String serverName, String documentRoot) throws VhostManagerException;
+
+
+    /**
+     * Delete a DocumentRoot directive in a Name-based Virtual Host
+     *
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @throws VhostManagerException
+     */
+    void deleteDocumentRoot(String address, String serverName) throws VhostManagerException;
+
+
+    /**
+     * Create a ServerAlias directive in a Virtual Host
+     *
+     *
+     * @param address address of the virtual host
+     * @param serverAlias argument(s) of the ServerAlias directive to create
+     * @throws VhostManagerException
+     */
+    void createServerAlias(String address, List<String> serverAlias) throws VhostManagerException;
+
+    /**
+     * Delete a ServerAlias directive in a Virtual Host
+     *
+     *
+     * @param address address of the virtual host
+     * @throws VhostManagerException
+     */
+    void deleteServerAlias(String address) throws VhostManagerException;
+
+    /**
+     * Create a ServerAlias directive in a Name-based Virtual Host
+     *
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @param serverAlias argument(s) of the ServerAlias directive to create
+     * @throws VhostManagerException
+     */
+    void createServerAlias(String address, String serverName, List<String> serverAlias) throws VhostManagerException;
+
+    /**
+     * Delete a ServerAlias directive in a Name-based Virtual Host
+     *
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @throws VhostManagerException
+     */
+    void deleteServerAlias(String address, String serverName) throws VhostManagerException;
+
+
+    /**
+     * Create a ServerPath directive in a Virtual Host
+     *
+     * @param address address of the virtual host
+     * @param serverPath value of the ServerPath directive to create
+     * @throws VhostManagerException
+     */
+    void createServerPath(String address, String serverPath) throws VhostManagerException;
+
+    /**
+     * Delete a ServerPath directive in a Virtual Host
+     *
+     *
+     * @param address address of the virtual host
+     * @throws VhostManagerException
+     */
+    void deleteServerPath(String address) throws VhostManagerException;
+
+    /**
+     * Create a ServerPath directive in a Name-based Virtual Host
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @param serverPath value of the ServerPath directive to create
+     * @throws VhostManagerException
+     */
+    void createServerPath(String address, String serverName, String serverPath) throws VhostManagerException;
+
+    /**
+     * Delete a ServerPath directive in a Name-based Virtual Host
+     *
+     *
+     * @param address address of the virtual host
+     * @param serverName value of the ServerName directive
+     * @throws VhostManagerException
+     */
+    void deleteServerPath(String address, String serverName) throws VhostManagerException;
+
+
+
+}
