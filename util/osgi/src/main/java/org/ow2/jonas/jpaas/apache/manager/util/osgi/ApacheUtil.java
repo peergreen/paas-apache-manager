7c61e3ebb32039b7c7901af861e933d5b47cee82
David Richard <david.richard@bull.net>
2012-05-11 11:18:41 +0200

    CLOUD-39 remove unused imports

--- util/osgi/src/main/java/org/ow2/jonas/jpaas/apache/manager/util/osgi/ApacheUtil.java
+++ util/osgi/src/main/java/org/ow2/jonas/jpaas/apache/manager/util/osgi/ApacheUtil.java
@@ -48,8 +48,6 @@ import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * The OSGi implementation of the ApacheUtilService Interface
